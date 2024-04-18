e204a38c445e61b43bed884f3292f0c421980a68
polinkar <pkaragiannopoulou@conceptmaniax.com>
2024-04-18 16:59:42 +0300

    tailor-made page

--- wp-content/themes/gaia/retreat.php
+++ wp-content/themes/gaia/retreat.php
@@ -10,7 +10,7 @@
     <picture class="hero__background">
       <img src="<?php echo get_field('image'); ?>" class="hero__background-img">
     </picture>
-    <div class="boxed-s centered hero__container">
+    <div class="boxed-s centered hero__container <?php if (get_field('white_texts')) : ?>white-text<?php endif; ?>">
       <h1 class="hero__heading heading-s italic serif">
         <?php echo get_field('heading'); ?>
       </h1>
@@ -45,8 +45,13 @@
         <?php while (have_rows('grid_item')) : the_row();
           $itemText = get_sub_field('item_text');
           $itemImage = get_sub_field('item_image');
+          $wideImageItem = get_sub_field('wide_image_item')
         ?>
-          <div class="main-grid__item two-col">
+          <div class="main-grid__item two-col 
+            <?php if ($wideImageItem) : ?>
+              main-grid__item--wide-image
+            <?php endif; ?>
+          ">
             <div class="main-grid__item-text-container">
               <div class="main-grid__item-title serif heading">
                 <?php echo $itemText['title']; ?>
@@ -74,8 +79,8 @@
               <?php endif; ?>
                 " alt="">
               <?php if ($itemImage['image_with_tiles']) : ?>
-                <img src="<?php echo $itemImage['image_small_left']; ?>" class="main-grid__item-image-sl" alt="">
-                <img src="<?php echo $itemImage['image_small_right']; ?>" class="main-grid__item-image-sr" alt="">
+                <img src="<?php echo $itemImage['image_small_left']; ?>" class="main-grid__item-image-s main-grid__item-image-sl" alt="">
+                <img src="<?php echo $itemImage['image_small_right']; ?>" class="main-grid__item-image-s main-grid__item-image-sr" alt="">
               <?php endif; ?>
             </div>
           </div>
