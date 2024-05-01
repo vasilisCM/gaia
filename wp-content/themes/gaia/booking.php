b4fa0ca804191e3ce4e421ab607d3914a117258d
vasilisCM <vpafiliaris@conceptmaniax.com>
2024-05-01 17:33:26 +0300

    booking contact form 7

--- wp-content/themes/gaia/booking.php
+++ wp-content/themes/gaia/booking.php
@@ -18,10 +18,20 @@
 
     <section class="online-booking">
         <div class="boxed-xs centered online-booking__container">
+
             <p class="online-booking__description">Fill in the information form with details such as the number of people, preferred
                 dates, and the purpose of your retreat. </p>
+            <div class="online-booking__text-container">
+                <h3>Retreat in Mykonos</h3>
+                <p>10-16 / 06</p>
+                <p>800€ deposit</p>
+            </div>
             <div class="online-booking__plugin">
-                <?php echo the_content(); ?>
+                <?php // echo the_content(); 
+                ?>
+                <div class="contact-form">
+                    <?php echo do_shortcode('[contact-form-7 id="7433ce2" title="Booking"]'); ?>
+                </div>
             </div>
         </div>
     </section>
