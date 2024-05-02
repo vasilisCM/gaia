4349c2bbe73317a8457a6f2934e718ffeab5e256
vasilisCM <vpafiliaris@conceptmaniax.com>
2024-05-02 14:47:48 +0300

    booking no semibold

--- wp-content/themes/gaia/booking.php
+++ wp-content/themes/gaia/booking.php
@@ -11,8 +11,8 @@
         $hero = get_field('hero');
         ?>
         <div class="intro__container">
-            <h1 class="intro__heading heading-ms sans-serif centered-text boxed-s centered semibold" animate="line"><?php echo $hero['heading']['text_large']; ?></h1>
-            <div class="intro__text  boxed-s centered centered-text text-l semibold" animate="line"><?php echo $hero['heading']['text_small']; ?></div>
+            <h1 class="intro__heading heading-ms sans-serif centered-text boxed-s centered" animate="line"><?php echo $hero['heading']['text_large']; ?></h1>
+            <div class="intro__text  boxed-s centered centered-text text-l light" animate="line"><?php echo $hero['heading']['text_small']; ?></div>
         </div>
     </section>
 
@@ -28,9 +28,9 @@
                 <?php
                 if (have_rows('booking__retreat')) : while (have_rows('booking__retreat')) : the_row(); ?>
                         <h3 class="heading serif online-booking__retreat"><?php echo get_sub_field('heading'); ?></h3>
-                        <p class="text-ml semibold"><?php echo get_sub_field('date'); ?></p>
-                        <p class="text semibold"><?php echo get_sub_field('comment'); ?></p>
-                        <div class="text semibold online-booking__options">
+                        <p class="text-ml"><?php echo get_sub_field('date'); ?></p>
+                        <p class="text"><?php echo get_sub_field('comment'); ?></p>
+                        <div class="text online-booking__options">
                             <p>Prices:</p>
                             <?php if (have_rows('prices')) : while (have_rows('prices')) : the_row();
                                     if (have_rows('options')) : while (have_rows('options')) : the_row(); ?>
